4c3940af8318a87d0cebdcd6d9a6446a0b5eb44a
Jiten Palaparthi <JPalaparthi@vmware.com>
2022-04-21 17:21:16 +0530

    Add Singleton and FactroyMethod patterns

---
+++ FactoryMethodExample/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace FactoryMethodExample
+{
+
+    public interface IProduct
+    {
+        string Hello();
+    }
+
+    abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+        public string SayHello()
+        {
+            var product = FactoryMethod();
+            var result = "Creator:" + product.Hello();
+            return result;
+        }
+
+    }
+//----------------------------------------------------------------------------------------------------
+    class ConcreteCreator1 : Creator
+    {
+
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreateProduct1();
+        }
+
+    }
+
+    class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreateProduct2();
+        }
+    }
+
+    class ConcreateProduct1 : IProduct
+    {
+        public string Hello()
+        {
+            return "Hello from Concreate Product1";
+        }
+    }
+
+    class ConcreateProduct2 : IProduct
+    {
+        public string Hello()
+        {
+            return "Hello from Concreate Product2";
+        }
+    }
+
+// ----------------------------------------------------------------------------------------------------
+    class Client
+    {
+        public void Main(int tp)
+        {
+            if (tp == 1)
+            {
+                Console.WriteLine("Calling ConcreteCreator1");
+                ClientCode(new ConcreteCreator1());
+            }
+            else
+            {
+                Console.WriteLine("Calling ConcreteCreator2");
+
+                ClientCode(new ConcreteCreator2());
+            }
+        }
+
+        public void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Client" + creator.SayHello());
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 50; i++)
+            {
+                Random rnd = new Random();
+                int num = rnd.Next();
+                new Client().Main(num % 2);
+            }
+        }
+    }
+}
---
+++ SingletonExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SingletonExample
+{ 
+    public sealed class Singleton{
+    private Singleton(){
+        Console.WriteLine("Calling the private constructer");
+    }
+
+public string constr {get;set;}
+      private static Singleton _instance;
+      public static Singleton GetInstance(string cstr){
+
+          if(_instance==null){
+              _instance = new Singleton();
+              _instance.ccstronstr= cstr;
+          }
+
+          return _instance;
+      }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance("dbconnection");
+            Console.WriteLine(s1.constr);
+            Singleton s2 = Singleton.GetInstance("sqlconnection");
+            Console.WriteLine(s2.constr);
+            Singleton s3 = Singleton.GetInstance("mysqlconnection");
+            Console.WriteLine(s3.constr);
+
+            if(s1==s2){
+                Console.WriteLine("Both the instances are same, hence they are Singleton");
+            }else{
+                Console.WriteLine("Not a single ton instance");
+
+            }
+        }
+    }
+}
+
+// Thread safe single ton pattern
